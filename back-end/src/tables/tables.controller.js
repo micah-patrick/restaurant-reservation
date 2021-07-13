e0a621ee46eb5c874bd0328178cacace4fa4b7fc
Micah <mbro36@gmail.com>
2021-07-13 12:57:27 -0600

    create table validations

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -7,6 +7,60 @@ async function create(req, res) {
     res.status(201).json({ data });
   }
 
+  const VALID_PROPERTIES = [
+    "table_name",
+    "capacity",
+  ];
+
+  function hasOnlyValidProperties(req, res, next) {
+    const { data = {} } = req.body;
+  
+    const invalidFields = Object.keys(data).filter(
+      (field) => !VALID_PROPERTIES.includes(field)
+    );
+  
+    if (invalidFields.length) {
+      return next({
+        status: 400,
+        message: `Invalid field(s): ${invalidFields.join(", ")}`,
+      });
+    }
+    next();
+  }
+
+  const hasRequiredProperties = hasProperties(...VALID_PROPERTIES);
+
+  function tableNameIsValid(req, res, next) {
+      const {table_name} = req.body.data;
+      const length = table_name.length;
+
+      if (length >= 2) {
+        return next();
+      }
+        return next({
+          status: 400,
+          message: `Invalid table_name field. table_name must be at least 2 characters long`
+        })
+  }
+
+  function capacityIsPositiveInteger(req, res, next) {
+    let {capacity} = req.body.data;
+  
+    if (capacity > 0 && Number.isInteger(capacity)) {
+      return next();
+    }
+      return next({
+        status: 400,
+        message: `Invalid capacity field. Capacity must be a positive integer greater than 0`
+      })
+  }
+
 module.exports = {
-    create: [asyncErrorBoundary(create)]
+    create: [
+        hasOnlyValidProperties,
+        hasRequiredProperties,
+        tableNameIsValid,
+        capacityIsPositiveInteger,
+        asyncErrorBoundary(create)
+    ]
 }
