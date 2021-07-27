ddf374aa5a843456f20f395691d8bc4d7c382f03
Micah <mbro36@gmail.com>
2021-07-27 16:44:19 -0600

    api fix

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,6 +5,8 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
+require("dotenv").config();
+
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
